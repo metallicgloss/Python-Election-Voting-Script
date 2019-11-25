19e3e13b7ef65e50e1c57810d344b1fe6d658487
William Phillips <metallicgloss@gmail.com>
2019-11-25 09:23:15 +0000

    Initial commit.

---
+++ mainApplication.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+import pygubu
+
+
+class votingApplication:
+    def __init__(self, master):
+        self.interfaceBuilder = interfaceBuilder = pygubu.Builder()
+        interfaceBuilder.add_from_file('designerFiles/mainMenu.ui')
+        self.mainMenu = interfaceBuilder.get_object('mainMenu', master)
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = votingApplication(root)
+    root.mainloop()
