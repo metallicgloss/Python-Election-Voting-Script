25b460906581ef008b01cc1c5bc13bdf427991db
William Phillips <metallicgloss@gmail.com>
2019-11-27 12:06:09 +0000

    Start of password hashing.

--- mainApplication.py
+++ mainApplication.py
@@ -1,12 +1,9 @@
 # Import required modules for program.
-import mysql.connector
+import os, mysql.connector, datetime, connectionString, pygubu, hashlib, binascii
 from mysql.connector import Error
 from dateutil.parser import parse
-import datetime
-import connectionString
 import tkinter as tk
 from tkinter import messagebox
-import pygubu
 
 # Module initialisation - open MySQL connection using connection query data, initialise cursor.
 try:
@@ -21,33 +18,53 @@ class Student:
         # Define class variables.
         self.username = username
         self.password = password
+        self._salt = ""
+        self._hashed_password = ""
         
     # Insert a new student voter to the system.
     def insert_new_student(self):
+        # Generate hash and salt.
+        self.generate_hash_password()
+        
         # Execute MySQL Query, substitute %s with values with student details.
-        mysql_cursor.execute("INSERT INTO `studentVoters` (`studentLogin`, `studentPassword`) VALUES (%s, %s)", [self.username, self.password])
+        mysql_cursor.execute("INSERT INTO `studentVoters` (`studentLogin`, `studentPassword`, `studentSalt`) VALUES (%s, %s, %s)", [self.username, self._hashed_password, self._salt])
         database_connection.commit()
         return mysql_cursor.lastrowid
     
-    # Perform verification on student login credentials to database.
-    def verify_student_details(self):
-        # Execute MySQL Query
-        mysql_cursor.execute("SELECT `studentLogin` FROM `studentVoters` WHERE `studentLogin` = '%s' AND `studentPassword` = '%s'", [self.username, self.password])
+    # Generate and store new hashed password
+    def generate_hash_password(self):
+        # Generate random salt key using hashlib, a random string generated.
+        self._salt = hashlib.sha256(os.urandom(64)).hexdigest().encode('ascii')
+        
+        # Generate hashed version of the password converted to hex.
+        self._hashed_password = binascii.hexlify(hashlib.pbkdf2_hmac('sha512', self.password.encode('utf-8'), self._salt, 100000))
+    
+    # Generate and return hashed password
+    def get_hashed_password(self):
+        return binascii.hexlify(hashlib.pbkdf2_hmac('sha512', self.password.encode('utf-8'), self._salt, 100000))
+    
+    # Verify password
+    def verify_password(self):
+        # Execute MySQL Query to get password and hash.
+        mysql_cursor.execute("SELECT `studentPassword`, `studentSalt` FROM `studentVoters` WHERE `studentLogin` = %s", [self.username])
         
         # Store query_result as all values returned.
         query_result = mysql_cursor.fetchall()
         
-        if(mysql_cursor.rowcount == 0):
-            # Query returned nothing, details do not match record.
-            return False
+        messagebox.showinfo('Success', query_result[1])
+        
+        self._hash = query_result['studentSalt']
+        
+        if(self.get_hashed_password() == query_result['studentPassword']):
+            messagebox.showinfo('Success', 'Yes.')
         else:
-            # Query returned data, user authenticated. Crude way of doing it, but acceptable for this task.
-            return True
-            
+            messagebox.showinfo('Success', 'No')
+                
+         
     # Verify username does not already exist.
     def verify_unique_username(self):
         # Execute MySQL Query, substitute %s with values with student username.
-        mysql_cursor.execute("SELECT `studentLogin` FROM `studentVoters` WHERE `studentLogin` = %s", [self.username])
+        mysql_cursor.execute("SELECT `studentLogin` FROM `studentVoters` WHERE `studentLogin` LIKE %s", [self.username])
         
         # Store query_result as all values returned.
         query_result = mysql_cursor.fetchall()
@@ -79,8 +96,8 @@ class Candidate:
         # Define class variables.
         self.name = name
         self.email = email
-        self.id = ""
-        self.position = ""
+        self._id = ""
+        self._position = ""
         
     # Insert a new candidate to the system.
     def insert_new_candidate(self):
@@ -112,12 +129,12 @@ class Candidate:
         ###
         
         # Execute MySQL Query
-        mysql_cursor.execute("SELECT `candidateID` FROM `gsuCandidates` WHERE `candidateName` = '%s'", [self.name])
+        mysql_cursor.execute("SELECT `candidateID` FROM `gsuCandidates` WHERE `candidateName` = %s", [self.name])
         
         # Store query_result as all values returned.
         query_result = mysql_cursor.fetchall()
         
-        self.id = query_result
+        self._id = query_result
         
         # Return ID of the candidate searched.
         return query_result
@@ -135,7 +152,7 @@ class Candidate:
             for line in election_positions.get_positions():
                 print("Position Available: Option " + str(line['positionID']) + " - " + line['positionTitle'])
 
-            mysql_cursor.execute("INSERT INTO `gsuCandidateApplication` (`candidateID`, `positionID`, `electionID`) VALUES (%s, %s, %s)", [self.id, input("Please enter the position you would like to apply for: "), election_id])
+            mysql_cursor.execute("INSERT INTO `gsuCandidateApplication` (`candidateID`, `positionID`, `electionID`) VALUES (%s, %s, %s)", [self._id, input("Please enter the position you would like to apply for: "), election_id])
             database_connection.commit()
             return mysql_cursor.lastrowid
             
@@ -149,7 +166,7 @@ class Election:
         # Define class variables.
         self.start_time = start_time
         self.end_time = end_time
-        self.id = ""
+        self._id = ""
         
     # Create a new election time period in the database.
     def create_election(self):
@@ -163,11 +180,11 @@ class Election:
     # Check status of any election curretly running.
     def get_current_election(self):
         # Execute MySQL Query
-        mysql_cursor.execute("SELECT `electionID` FROM `gsuElection` WHERE `electionStartTime` < '%s' AND `electionEndTime` > '%s'", [parse(datetime.datetime.now())])
+        mysql_cursor.execute("SELECT `electionID` FROM `gsuElection` WHERE `electionStartTime` < %s AND `electionEndTime` > %s", [parse(datetime.datetime.now())])
         
         # Store query_result as all values returned.
         query_result = mysql_cursor.fetchall()
-        self.id = query_result
+        self._id = query_result
         
         if(mysql_cursor.rowcount == 0):
             # No election currently running.
@@ -179,7 +196,7 @@ class Election:
     # Get all candidates running in the election.
     def get_all_candidates(self):
         # Execute MySQL Query
-        mysql_cursor.execute("SELECT * FROM `gsuCandidateApplications` WHERE `electionID` = '%s'", [self.id])
+        mysql_cursor.execute("SELECT * FROM `gsuCandidateApplications` WHERE `electionID` = '%s'", [self._id])
         
         # Store query_result as all values returned.
         query_result = mysql_cursor.fetchall()
@@ -217,6 +234,7 @@ class Position:
         election = Election()
         election_id = election.get_current_election()
         
+        # Select all positions where they're not in the list containing positions applied for more than 4 times in a given election.
         mysql_cursor.execute("SELECT * FROM `gsuPositions` WHERE `positionID` NOT IN (SELECT `positionID` FROM `gsuCandidateApplications` WHERE `electionID` = '%s' AND COUNT(SELECT `positionID` FROM `gsuCandidateApplications` WHERE `electionID` = '%s') < 5)", [election_id, election_id])
         
         # Store query_result as all values returned.
@@ -316,8 +334,9 @@ class voting_application(pygubu.TkApplication):
         if ((username != "") and (password != "")):
             # If input is not blank, create user.
             new_student = Student(username, password)
+            new_student.generate_hash_password()
             
-            if(new_student.verify_unique_username()):
+            if(True):
                 #Verify that the username hasn't been used before. If unique, insert student.
                 new_student.insert_new_student()
                 messagebox.showinfo('Success', 'Created successfully.')
@@ -360,7 +379,7 @@ class voting_application(pygubu.TkApplication):
     #                      Create Candidate                     #
     #############################################################
     
-    # Create new election period on the system.
+    # Create new candidate that can make applications.
     def create_candidate(self):
         # Get user input from the page.
         name = self.interfaceBuilder.get_object('candidate_name_txtbx').get()
@@ -369,7 +388,7 @@ class voting_application(pygubu.TkApplication):
         if ((name != "") and (email != "")):
             new_candidate = Candidate(name, email)
             
-            if(verify_unique_name()):
+            if(new_candidate.verify_unique_name()):
                 # Check there isn't already a candidate with same name.
                 new_candidate.insert_new_candidate()
                 messagebox.showinfo('Success', 'Created successfully.')
@@ -381,6 +400,30 @@ class voting_application(pygubu.TkApplication):
         else:
             # Else change label text to error message.
             self.interfaceBuilder.get_object('create_candidate_error_lbl').configure(text="Please ensure you've entered data into both boxes.")
+  
+    #############################################################
+    #                       Student Login                       #
+    #############################################################
+    
+    # Perform student login verification.
+    def student_login(self):
+        # Get user input from the page.
+        username = self.interfaceBuilder.get_object('student_login_username_txtbx').get()
+        password = self.interfaceBuilder.get_object('student_login_password_txtbx').get()
+        
+        if ((username != "") and (password != "")):
+            student_login = Student(username, password)
+            student_login.verify_password()
+            
+            if(student_login.verify_student_details()):
+                self.change_frame('student_vote_frame')
+            else:
+                # If not unique, inform user to add custom tag to surname (other option is to change name) to help voters.
+                self.interfaceBuilder.get_object('student_login_error_lbl').configure(text="Uh Oh! Wrong username or password!")
+            
+        else:
+            # Else change label text to error message.
+            self.interfaceBuilder.get_object('student_login_error_lbl').configure(text="Please ensure you've entered data into both boxes.")
   
 if __name__ == '__main__':
     tkinter_app = tk.Tk()
