231612772ea32026af0af86d3f38bd6bb11b0e57
William Phillips <metallicgloss@gmail.com>
2020-01-27 17:49:25 +0000

    Submitted.

--- connectionString.py
+++ connectionString.py
@@ -1,4 +1,4 @@
-dbHost = ""
-dbDatabase = ""
-dbUsername = ""
-dbPassword = ""
+dbHost = "5.226.143.50"
+dbDatabase = "metallic_1811_coursework"
+dbUsername = "metallic_1811"
+dbPassword = "hTpN2r53WesnFaP3n6"
